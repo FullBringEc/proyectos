ec0e2361aeb2f83e78ec9255d33e892e3b477778
Adrian <adrianmorales_15@hotmail.com>
2018-01-03 17:32:36 -0500

    Modulos innecesarios
    Signed-off-by: Adrian <adrianmorales_15@hotmail.com>

--- todo_ui/__openerp__.py
+++
@@ -1,6 +0,0 @@
-{ 'name': 'User interface improvements to the To-Do app',
-  'description': 'User friendly features.',
-  'author': 'Daniel Reis',
-  'depends': ['todo_user'],
-  'data': ['todo_view.xml']
-}
